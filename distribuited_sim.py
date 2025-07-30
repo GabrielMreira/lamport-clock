02630ba6c8cc3ac3b0298b083d89cbd8d6ad2951
Gabriel Moreira <107933147+GabrielMreira@users.noreply.github.com>
2025-07-29 21:43:27 -0300

    lamport-clock: finalização do envio de mensagem e inicio do processo de salvar snapchot

--- distribuited_sim.py
+++ distribuited_sim.py
@@ -96,3 +96,52 @@ class DistribuitedProcess:
                 client_socket.close()
 
     def _process_received_message(self, message : str):
+        parts = message.strip().split(':',3)
+
+        ts_received, origin_id, mesage_type, content = int(parts[0]), int(parts[1]), parts[2], parts[3]
+
+        with self.lock:
+            self.lamport_clock = max(self.lamport_clock, ts_received) + 1
+            self._log(f"Received '{mesage_type}' from P{origin_id} [TS: {ts_received}]")
+
+            if mesage_type == 'MARKER':
+                if not self.is_capturing:
+                    self._start_snapshot_procedure(initiated_by_marker=True)
+                self.received_markers_from[origin_id] = True
+            else:
+                if self.is_capturing and not self.received_markers_from.get(origin_id, True):
+                    if origin_id not in self.in_transit_messages:
+                        self.in_transit_messages[origin_id] = []
+
+                    self.in_transit_messages[origin_id].append(message)
+                    self._log(f"Message from P{origin_id} in transit")
+
+                    if self.is_capturing and all(self.received_markers_from.values()):
+                        self._finalize_snapshot()
+
+    def _start_snapshot_procedure(self, initiated_by_marker=False):
+        self.is_capturing = True
+        self.snapshot_state = self.local_state
+        self.in_transit_messages = {}
+
+        for peer_id in self.others_peers:
+            self.received_markers_from[peer_id] = False
+
+        if initiated_by_marker:
+            self._log("SNAPSHOT INITIATED FROM MARKER")
+        else:
+            self._log("SNAPSHOT INITIATED AS MARKER")
+
+        threading.Thread(target=self._broadcast_markers).start()
+
+    def _internal_event(self):
+        with self.lock:
+            self.lamport_clock += 1
+            self.local_state += 1
+            self._log("Execute internal event")
+
+    def _broadcast_marker(self):
+        for peer_id in self.others_peers:
+            self._send_message(peer_id, 'MARKER')
+
+    def _finalize_snapshot(self):
